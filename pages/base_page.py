db291c6816a159daec1ba3476a81eb407e140f56
dante7928502 <dante7928502@mail.ru>
2026-01-14 12:29:04 +0300

    4.3.5 added 2 negative check methods into base_page and product_page, for future use

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from math import log, sin
 
 
@@ -16,10 +20,27 @@ class BasePage:
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return True
+        return False
     
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -32,4 +53,4 @@ class BasePage:
             print(f"Your code: {alert_text}")
             alert.accept()
         except NoAlertPresentException:
-            print("No second alert presented")
+            print("No second alert presented after solving quiz, so we can't get code")
--- pages/product_page.py
+++ pages/product_page.py
@@ -22,3 +22,13 @@ class ProductPage(BasePage):
 
     def find_basket_price(self):
         return self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE
+        ), "Success message is presented, but should not be"
+
+    def success_msg_is_dissapeared(self):
+        assert self.is_disappeared(
+            *ProductPageLocators.SUCCESS_MESSAGE
+        ), "Success message should dissapear, but it's still here"
